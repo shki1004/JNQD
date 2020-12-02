16123c32ad3a4440acf3eeeb46698922623f7383
Sehwan Ki <shki@kaist.ac.kr>
2020-12-02 17:04:14 +0900

    Add files via upload

---
+++ enc_file/enc.bat
@@ -0,0 +1,1 @@
+TAppEncoder_HM16.17.exe -c encoder_intra_main.cfg -c seq.cfg -i video.yuv -q 22 -b video_QP22_AI_Direct.bin -o video_QP22_AI_Direct.yuv >> video_QP22_AI_Direct.txt
---
+++ enc_file/enc_Bae_PVC.bat
@@ -0,0 +1,1 @@
+TAppEncoder_Bae_PVC_HM16.17.exe -c encoder_intra_main.cfg -c seq.cfg -i video.yuv -q 22 -b video_QP22_AI_Bae.bin -o video_QP22_AI_Bae.yuv >> video_QP22_AI_Bae.txt
